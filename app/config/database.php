eef659bf3cf844ca4fe97af56448d9526b626363
Kristian Lauttamus <krizzuq@hotmail.com>
2014-11-01 14:18:24 +0200

    Configs

--- app/config/database.php
+++ app/config/database.php
@@ -55,8 +55,8 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
+			'database'  => 'projectmanager9k',
+			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
