8cab670771ae1c9ce83df9e2b05869fd74ab6acd
Kristian Lauttamus <krizzuq@hotmail.com>
2014-11-01 14:18:14 +0200

    Lang
    Language files for login and register

---
+++ app/lang/fi/login.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Login Language Lines
+	|--------------------------------------------------------------------------
+	*/
+
+	'signin' => 'Kirjaudu sisään',
+	'signmein' => 'Kirjaudu sisään',
+
+	'email' => 'Sähköpostiosoite',
+	'password' => 'Salasana',
+	'rememberme' => 'Muista minut',
+
+	'registernew' => 'Rekisteröi uusi tunnus',
+
+);
---
+++ app/lang/fi/register.php
@@ -0,0 +1,21 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Register Language Lines
+	|--------------------------------------------------------------------------
+	*/
+
+	'registerme' => 'Luo tunnus',
+	'register' => 'Rekisteröidy',
+	'registered' => 'Minulla on jo tunnus',
+
+	'firstname'     => 'Etunimi',
+	'lastname'     => 'Sukunimi',
+	'email'     => 'Sähköpostiosoite',
+	'password' => 'Salasana',
+	'passwordconfirmation' => 'Toista salasana',
+
+);
